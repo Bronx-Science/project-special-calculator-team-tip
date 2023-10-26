014ceb43c66d79dc3ad5dd9d063a3684a37b9ac4
Matthew Chi <chim@bxscience.edu>
2023-10-26 09:26:25 -0400

    Added Our Files to This Repository

---
+++ tip_calculator/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:tip_calculator/calculator_view.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tip Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CalculatorView(),
+    );
+  }
+}
