52c44bce7df6e2a7bdd51e8af06f021d88b736f6
Matthew Chi <chim@bxscience.edu>
2023-10-29 14:47:16 -0400

    Friday's Edits

--- tip_calculator/lib/calculator_view.dart
+++ tip_calculator/lib/calculator_view.dart
@@ -21,85 +21,18 @@ class _CalculatorViewState extends State<CalculatorView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                onChanged: (val) {
-                  bill = double.parse(val);
-                },
-                decoration: const InputDecoration(hintText: 'Bill amount'),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: <Widget>[
-                  const Text('Custom Tip %'),
-                  const Spacer(),
-                  GestureDetector(
-                      onTap: () {}, child: Icon(Icons.remove_circle)),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text('$tip %'),
-                  GestureDetector(onTap: () {}, child: Icon(Icons.add_circle)),
-                  SizedBox(
-                    width: 8,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-            ],
-            Row(
-              children: <Widget>[
-                Text('% Tax'),
-                Spacer(),
-                GestureDetector(onTap: () {}, child: Icon(Icons.remove_circle)),
-                SizedBox(
-                  width: 8,
-                ),
-                Text('$tax %'),
-                SizedBox(
-                  width: 8,
-                ),
-                GestureDetector(onTap: () {}, child: Icon(Icons.add_circle)),
-                SizedBox(
-                  width: 8,
-                ),
-              ],
+      appBar: AppBar(title: const Text("Tip Calculator")),
+      body: const Column(
+        children: <Widget>[
+          Text("Welcome to Tip Calculator",
+            style:  TextStyle(
+              color: Colors.black,
+              fontSize: 20.0,
             ),
-            SizedBox(
-              height: 40,
-            ),
-            GestureDetector(
-              onTap: () {
-                if (bill > 0) {
-                  calculate();
-                }
-              },
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-                decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(24)),
-                child: Text(
-                  'Calculate!',
-                  style: TextStyle(color: Colors.white, fontSize: 17),
-                ),
-              ),
-            ),
-            SizedBox(height: 16,),
-            tip != 0 ? Text('Tip $tip') : Container(),
-            SizedBox(
-              height: 8,
-            ),
-            total != 0 ? Text('Total Amount: $total')): Container
+          ),
+        ],
       ),
+      
     );
   }
 }
