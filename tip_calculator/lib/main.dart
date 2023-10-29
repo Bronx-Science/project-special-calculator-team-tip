2ef595c6f7314b44f0f6493a9c39ca4587efd2cc
Matthew Chi <chim@mit.edu>
2023-10-29 18:06:54 -0400

    Matthew's Start
    Made Scaffold Widget with
    Column Child and
    
    Form Class with
    TextFormField children
    
    TextFormFields have validation, and submit button uses controller to retrieve values

--- tip_calculator/lib/main.dart
+++ tip_calculator/lib/main.dart
@@ -1,20 +1,118 @@
-import 'package:tip_calculator/calculator_view.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Welcome to Tip Calculator!'),
+              //Spacer(flex: 1),
+              MyForm()
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
+
+//FORM
+
+class MyForm extends StatefulWidget {
+  const MyForm({Key? key}) : super(key:key);
+
+  @override
+  _MyFormState createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  double bill=0.0;
+  double tipPercent=0.0;
+  double taxPercent=0.0;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final billController = TextEditingController(); //Used to Retrive data inputted into text fields
+  final tipController = TextEditingController();
+  final taxController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tip Calculator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const CalculatorView(),
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget> [
+          const Text("Bill:"),
+          TextFormField(
+            decoration: const InputDecoration(    //Adds a Border
+              border: OutlineInputBorder()
+            ),
+            validator: (String? value) {            //Check if Field is Blank
+              if(value == null || value.isEmpty) {
+                return 'Please enter a bill';
+              }
+              return null;
+            },
+            controller: billController
+          ),
+          const Text("Tip %: "),
+          TextFormField(
+            decoration: const InputDecoration(    //Adds a Border
+              border: OutlineInputBorder()
+            ),
+            validator: (String? value) {            //Check if Field is Blank
+              if(value == null || value.isEmpty) {
+                return 'Please enter a tip';
+              }
+              return null;
+            },
+            controller: tipController
+          ),
+          const Text("Tax %: "),
+          TextFormField(
+            decoration: const InputDecoration(    //Adds a Border
+              border: OutlineInputBorder()
+            ),
+            validator: (String? value) {            //Check if Field is Blank
+              if(value == null || value.isEmpty) {
+                return 'Please enter a tax';
+              }
+              return null;
+            },
+            controller: taxController
+          ),
+          Center(child:
+            ElevatedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState!.validate()) {
+                // Process data.
+                bill = double.parse(billController.text);
+                tipPercent = double.parse(tipController.text)/100;
+                taxPercent = double.parse(taxController.text)/100;
+                }
+                print(bill);
+                print(tipPercent);
+                print(taxPercent);
+              },
+              child: const Text('Submit'),
+            )
+          )
+        ]
+      )
     );
   }
 }
