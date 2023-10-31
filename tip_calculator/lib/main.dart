755d54acc7b3018ef4d08389dcb6f3c5e981be11
Matthew Chi <chim@bxscience.edu>
2023-10-31 09:44:32 -0400

    Update main.dart

--- tip_calculator/lib/main.dart
+++ tip_calculator/lib/main.dart
@@ -11,16 +11,20 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
+
+
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text('Welcome to Tip Calculator!'),
               //Spacer(flex: 1),
               MyForm()
+              //placeholder widget
             ],
           ),
         )
@@ -46,52 +50,74 @@ class _MyFormState extends State<MyForm> {
   final billController = TextEditingController(); //Used to Retrive data inputted into text fields
   final tipController = TextEditingController();
   final taxController = TextEditingController();
+  List<bool> selection = [true, false, false];
+   String total = '0.00';
 
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget> [
           const Text("Bill:"),
-          TextFormField(
-            decoration: const InputDecoration(    //Adds a Border
-              border: OutlineInputBorder()
+          SizedBox(
+            width: 200,
+            child: TextFormField(
+              decoration: const InputDecoration(    //Adds a Border
+                border: OutlineInputBorder()
+              ),
+              style: const TextStyle(fontSize: 20, height: 2, color:Colors.black),
+              validator: (String? value) {            //Check if Field is Blank
+                if(value == null || value.isEmpty || double.tryParse(value) == null) {
+                  return 'Please enter a bill';
+                }
+                return null;
+              },
+              controller: billController
             ),
-            validator: (String? value) {            //Check if Field is Blank
-              if(value == null || value.isEmpty) {
-                return 'Please enter a bill';
-              }
-              return null;
-            },
-            controller: billController
           ),
           const Text("Tip %: "),
-          TextFormField(
+          SizedBox(
+            width: 200,
+            child: TextFormField(
             decoration: const InputDecoration(    //Adds a Border
               border: OutlineInputBorder()
             ),
+            style: const TextStyle(fontSize: 20, height: 2, color:Colors.black),
             validator: (String? value) {            //Check if Field is Blank
-              if(value == null || value.isEmpty) {
+              if(value == null || value.isEmpty || double.tryParse(value) == null) {
                 return 'Please enter a tip';
               }
               return null;
             },
             controller: tipController
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ToggleButtons(
+            isSelected: selection,
+            onPressed: updateSelection,
+            children: const [Text('10%'), Text('15%'), Text('20%')],
+            ),
           ),
           const Text("Tax %: "),
-          TextFormField(
-            decoration: const InputDecoration(    //Adds a Border
-              border: OutlineInputBorder()
+          SizedBox(
+            width: 200,
+            child: TextFormField(
+              decoration: const InputDecoration(    //Adds a Border
+                border: OutlineInputBorder()
+              ),
+              validator: (String? value) {            //Check if Field is Blank
+                if(value == null || value.isEmpty || double.tryParse(value) == null) {
+                  return 'Please enter a tax';
+                }
+                return null;
+              },
+              controller: taxController
             ),
-            validator: (String? value) {            //Check if Field is Blank
-              if(value == null || value.isEmpty) {
-                return 'Please enter a tax';
-              }
-              return null;
-            },
-            controller: taxController
           ),
           Center(child:
             ElevatedButton(
@@ -100,9 +126,7 @@ class _MyFormState extends State<MyForm> {
                 // the form is invalid.
                 if (_formKey.currentState!.validate()) {
                 // Process data.
-                bill = double.parse(billController.text);
-                tipPercent = double.parse(tipController.text)/100;
-                taxPercent = double.parse(taxController.text)/100;
+                  calculateTotal();
                 }
                 print(bill);
                 print(tipPercent);
@@ -110,9 +134,42 @@ class _MyFormState extends State<MyForm> {
               },
               child: const Text('Submit'),
             )
-          )
-        ]
+          ),
+          if (total !='0.00') // Check if 'tip' is not '0.00' before displaying
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                total, // Show the '$' symbol here
+                style: const TextStyle(fontSize: 30),
+              ),
+            ),
+        ],
       )
     );
+  }
+  void updateSelection(int selectedIndex) {
+    setState(() {
+      for (int i = 0; i < selection.length; i++) {
+        selection[i] = selectedIndex == i;
+      }
+    });
+  }
+
+  void calculateTotal() {
+    final bill = double.parse(double.parse(billController.text).toStringAsPrecision(2));
+    if (tipController.text != null) {
+      final tipPercent = double.parse((double.parse(tipController.text)/100).toStringAsPrecision(2));
+    }
+    else {
+      final selectedIndex = selection.indexWhere((element) => element);
+      final tipPercent = [0.1, 0.15, 0.2][selectedIndex];
+    }
+    final taxPercent = double.parse((double.parse(taxController.text)/100).toStringAsPrecision(2));
+
+    final totalAmount = (bill + (bill * tipPercent) + (bill*taxPercent)).toStringAsFixed(2);
+
+    setState(() {
+      total = '\$$totalAmount';
+    });
   }
 }
